80b380c87430f894eafc2f7c7981a71ecceede40
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-11-01 16:53:17 -0600

    refactor: Convert to promise await

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ export async function handler() {
   const client = new AWS.DynamoDB();
 
   try {
-    const results = client.getItem(
-      {
+    const results = await client
+      .getItem({
         TableName: "Attachments",
         Key: AWS.DynamoDB.Converter.marshall({
           REC_ID: "01H59D67PV21TDNK9Z5K5ACZJ3",
         }),
-      },
-      function (err, data) {
-        if (err) {
-          console.debug("WE HAD ERROR!!!!");
-          console.debug(err);
-        } else {
-          console.debug("Data is... ");
-          console.debug(data);
-        }
-      },
-    );
+      })
+      .promise();
+
+    console.debug("results = ");
+    console.debug(results);
   } catch (err) {
     console.debug("ERROR!!!!");
     console.debug(err);
