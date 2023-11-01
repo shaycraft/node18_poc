9c8808a1d49e5e38c6126c25b83a63d009a2ecf9
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-11-01 12:20:22 -0600

    feat: dynamodb call

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-exports.handler = () => {
-  console.debug("wazzup!!!");
-};
+import * as AWS from "aws-sdk";
 
-module.exports.handler();
+exports.handler = async () => {
+  console.debug("wazzup!!!");
+  const client = new AWS.DynamoDB.DocumentClient();
+  const results = await client.get({
+    TableName: "Attachments",
+    Key: {
+      REC_ID: "01H59D67PV21TDNK9Z5K5ACZJ3",
+    },
+  });
+};
