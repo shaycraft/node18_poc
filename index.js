975d993c9cf228b3657d4f54f995e6dcd7559657
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-11-01 12:28:52 -0600

    feat: error handling and printing results

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ import * as AWS from "aws-sdk";
 exports.handler = async () => {
   console.debug("wazzup!!!");
   const client = new AWS.DynamoDB.DocumentClient();
-  const results = await client.get({
-    TableName: "Attachments",
-    Key: {
-      REC_ID: "01H59D67PV21TDNK9Z5K5ACZJ3",
-    },
-  });
+
+  try {
+    const results = await client.get({
+      TableName: "Attachments",
+      Key: {
+        REC_ID: "01H59D67PV21TDNK9Z5K5ACZJ3",
+      },
+    });
+
+    console.debug(results.json());
+  } catch (err) {
+    console.debug("ERROR!!!!");
+    console.debug(err);
+  }
 };
+
+// module.exports.handler();
