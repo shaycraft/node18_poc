ac720bd823058f3b31150d60bd5824b643a90d7d
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-11-01 16:50:59 -0600

    feat: cleanup to dynamoDB call

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 import { default as AWS } from "aws-sdk";
 
 export async function handler() {
-  console.debug("wazzup!!!");
-  // console.debug(AWS.default.DynamoDB);
   const client = new AWS.DynamoDB();
 
   try {
-    console.debug(Object.keys(client));
     const results = client.getItem(
       {
         TableName: "Attachments",
-        Key: {
-          REC_ID: { S: "01H59D67PV21TDNK9Z5K5ACZJ3" },
-        },
+        Key: AWS.DynamoDB.Converter.marshall({
+          REC_ID: "01H59D67PV21TDNK9Z5K5ACZJ3",
+        }),
       },
       function (err, data) {
         if (err) {
-          console.debug("WE HAD ERRRO!!!!");
+          console.debug("WE HAD ERROR!!!!");
           console.debug(err);
         } else {
           console.debug("Data is... ");
@@ -24,13 +21,6 @@ export async function handler() {
         }
       },
     );
-
-    // console.debug(results);
-    // console.debug(Object.keys(results.response));
-    // console.debug(results.response);
-    // console.debug(results);
-    console.debug(AWS.DynamoDB.Converter.marshall({ foo: "bar" }));
-    // console.debug(Object.keys(AWS));
   } catch (err) {
     console.debug("ERROR!!!!");
     console.debug(err);
